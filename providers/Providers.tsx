13f0ca6e6dc5fb120010de65324d00e405ffd51a
Ali Bayat Mokhtari <ali.bayatmokhtari96@gmail.com>
2024-09-04 13:10:30 +0330

    init chakra ui

---
+++ providers/Providers.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import { ChakraProvider } from "@chakra-ui/react";
+import { PropsWithChildren } from "react";
+
+export function Providers({ children }: PropsWithChildren) {
+  return <ChakraProvider>{children}</ChakraProvider>;
+}
